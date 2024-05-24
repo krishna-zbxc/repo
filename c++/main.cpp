a852d80d9a730adc6aaa74a6855785db352c3f5c
kk <164781673+krishna-zbxc@users.noreply.github.com>
2024-05-24 19:29:55 +0000

    new code

---
+++ c++/main.cpp
@@ -0,0 +1,22 @@
+// C++ program to find the sum of first n natural numbers
+// positive integers such as 1,2,3,...n are known as natural numbers
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i=1;
+    int sum=0;
+
+    
+    
+
+    for (int i = 1; i <= 5; ++i) {
+      cout << i << " "   ;
+    }
+
+    
+
+    return 0;
+}
